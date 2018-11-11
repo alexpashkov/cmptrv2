98250069ce0696a3fe8f0eb1ca1067b62940b9a0
Alexander Pashkov <alexpashkov123@gmail.com>
2018-11-12 00:50:07 +0200

    add eval-expr

--- src/cmptrv2/parser.clj
+++ src/cmptrv2/parser.clj
@@ -4,7 +4,7 @@
 (def parse
   (insta/parser
    "expr = term '+' expr | term '-' expr | term
-    term = factor '*' term |factor '/' term | factor
+    term = factor '*' term | factor '/' term | factor '%' term | factor
     factor = <'('> expr <')'> | num
     num = <#'\\s*'> #'-?\\d+(\\.\\d+)*' <#'\\s*'> 
    "))
@@ -12,8 +12,23 @@
 ;; op = <#'\\s*'> ( '*' | '**' | '^' | '/' | '%' | '+' | '-' ) <#'\\s*'>
 ;; num = <#'\\s*'> #'-?\\d+(\\.\\d+)*' <#'\\s*'> 
 
-(->> "1*(1+(((100))))"
-     (parse)
-     (insta/transform {:expr (fn [& [v :as args]]
-                               (if (= 1 (count args)) v args))
-                       :num read-string}))
+(def ops {"+" +
+          "-" -
+          "*" *
+          "/" /
+          "%" mod})
+
+(defmulti eval-expr first)
+
+(defmethod eval-expr :factor [[_ x :as args]]
+  (eval-expr x))
+
+(defmethod eval-expr :num [[_ x :as args]]
+  (read-string x))
+
+(defmethod eval-expr :default [[_ x op y :as args]]
+  (if (= 2 (count args))
+    (eval-expr x)
+    (apply (ops op) (map eval-expr [x y]))))
+
+(eval-expr (parse "(10+5)*10+10"))
